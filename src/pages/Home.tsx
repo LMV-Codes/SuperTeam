96b03cd9f42013ab0fa72598a221cf3cf0a8b8d1
lmv <vazquez.leo.m@gmail.com>
2021-04-28 11:52:46 -0300

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import { Flex } from "@chakra-ui/layout";
+import React from "react";
+
+export const Home: React.FC = () => {
+  return <Flex>HomePage</Flex>;
+};
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NotFound: React.FC = () => {
+  return (
+    <Flex flexDirection="column" textAlign="center" marginTop="5em">
+      <Heading>404 - Page not found</Heading>
+      <Text>Sorry the page you're trying to reach doesn't exist</Text>
+      <Link to="/">click here to go back to homepage</Link>
+    </Flex>
+  );
+};
