7b0d610da8586f6839ee390fdacded439222c051
lmv <vazquez.leo.m@gmail.com>
2021-05-04 18:15:08 -0300

    Round height/weight

--- src/components/heroes/TeamHW.tsx
+++ src/components/heroes/TeamHW.tsx
@@ -10,14 +10,18 @@ export const TeamHW: React.FC<TeamHWProps> = ({ team }) => {
   const getAverageHeight = (team: HeroData[]) => {
     let heights: Array<number> = [];
     team.forEach((hero) => heights.push(parseInt(hero.appearance.height[1])));
-    const avgHeight = heights.reduce((a, b) => a + b, 0) / heights.length;
+    const avgHeight = Math.round(
+      heights.reduce((a, b) => a + b, 0) / heights.length
+    );
     return avgHeight;
   };
 
   const getAverageWeight = (team: HeroData[]) => {
     let weights: Array<number> = [];
     team.forEach((hero) => weights.push(parseInt(hero.appearance.weight[1])));
-    const avgWeight = weights.reduce((a, b) => a + b, 0) / weights.length;
+    const avgWeight = Math.round(
+      weights.reduce((a, b) => a + b, 0) / weights.length
+    );
     return avgWeight;
   };
 
