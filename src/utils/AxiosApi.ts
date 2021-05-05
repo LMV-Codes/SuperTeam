e042720039cf618c1b50ed8afadd1539817556f5
lmv <vazquez.leo.m@gmail.com>
2021-05-05 16:25:59 -0300

    Removed cors anywhere proxy

--- src/utils/AxiosApi.ts
+++ src/utils/AxiosApi.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 const fbToken = "10223511453527327";
 
 export const AxiosApi = axios.create({
-  baseURL: `https://cors-anywhere.herokuapp.com/superheroapi.com/api/${fbToken}/`,
+  baseURL: `https://superheroapi.com/api/${fbToken}/`,
 });
