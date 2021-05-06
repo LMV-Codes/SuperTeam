a7577679e0cd5e99e0bf22a47c51b1c8b14cf7d1
lmv <vazquez.leo.m@gmail.com>
2021-05-06 17:25:58 -0300

    final footer fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,48 +5,50 @@ import { FaLinkedinIn, FaBriefcase, FaGithub } from "react-icons/fa";
 
 export const Footer: React.FC = () => {
   return (
-    <Flex
-      bg="brand.50"
-      height="5em"
-      justifyContent="center"
-      alignContent="center"
-      flexDirection="column"
-      position="relative"
-      width="100%"
-      bottom="0"
-    >
-      <Text fontFamily="Roboto Condensed" textAlign="center">
-        Made by: LMV-Codes
-      </Text>
-      <Flex justifyContent="center" margin="0.5em">
-        <Link
-          isExternal
-          href="https://www.linkedin.com/in/leonardo-vazquez-a89660147/"
-        >
-          <Icon
-            as={FaLinkedinIn}
-            fontSize="1.5em"
-            _hover={{ color: "brand.300" }}
-          />
-        </Link>
-        <Link
-          marginLeft="1em"
-          isExternal
-          href="https://lv-portfolio.netlify.app/"
-        >
-          <Icon
-            as={FaBriefcase}
-            fontSize="1.5em"
-            _hover={{ color: "brand.300" }}
-          />
-        </Link>
-        <Link marginLeft="1em" isExternal href="https://github.com/LMV-Codes">
-          <Icon
-            as={FaGithub}
-            fontSize="1.5em"
-            _hover={{ color: "brand.300" }}
-          />
-        </Link>
+    <Flex marginTop="5em">
+      <Flex
+        bg="brand.50"
+        height="5em"
+        justifyContent="center"
+        alignContent="center"
+        flexDirection="column"
+        position="absolute"
+        width="100%"
+        bottom="0"
+      >
+        <Text fontFamily="Roboto Condensed" textAlign="center">
+          Made by: LMV-Codes
+        </Text>
+        <Flex justifyContent="center" margin="0.5em">
+          <Link
+            isExternal
+            href="https://www.linkedin.com/in/leonardo-vazquez-a89660147/"
+          >
+            <Icon
+              as={FaLinkedinIn}
+              fontSize="1.5em"
+              _hover={{ color: "brand.300" }}
+            />
+          </Link>
+          <Link
+            marginLeft="1em"
+            isExternal
+            href="https://lv-portfolio.netlify.app/"
+          >
+            <Icon
+              as={FaBriefcase}
+              fontSize="1.5em"
+              _hover={{ color: "brand.300" }}
+            />
+          </Link>
+          <Link marginLeft="1em" isExternal href="https://github.com/LMV-Codes">
+            <Icon
+              as={FaGithub}
+              fontSize="1.5em"
+              _hover={{ color: "brand.300" }}
+            />
+          </Link>
+        </Flex>
       </Flex>
     </Flex>
   );
